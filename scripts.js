192331b585f54f7d320dc43f0629b6a4d15e9734
ryansalty <rsoety@gmail.com>
2022-10-14 14:45:17 -0400

    Implement win-con function and buttons
    Clear board button resets game. New player button has no functionality
    yet. Upon a player winning, the game board is disabled to force user to
    reset the game.

--- scripts.js
+++ scripts.js
@@ -4,6 +4,7 @@ const gameBoard = (() => {
     const gameArray = new Array(9);
     const gameMessage = document.getElementById("msg"); 
     const gameTiles = document.getElementsByClassName("tile");
+    const playArea = document.getElementById("game-board");
     let playerSymbol = 'X';
     let opponentSymbol = 'O';
     console.log(gameTiles);
@@ -12,6 +13,9 @@ const gameBoard = (() => {
         gameArray.length = 0;
         gameArray.length = 9;
         gameMessage.innerHTML = "Welcome to the game!";
+        printBoard();
+        playArea.classList.remove("disabled");
+
     }
 
     const playerMove = (i) => {
@@ -21,22 +25,20 @@ const gameBoard = (() => {
             gameMessage.innerHTML = "Please make your selection";
             console.log(gameArray);
         }
+        checkWin();
     }
 
     const opponentMove = () => {
         let success = false;
         let i = 0;
-        let iteration = 1;
         while (success === false){
-            i = Math.floor(Math.random() * 10);
+            i = Math.floor(Math.random() * 9);
             if (gameArray[i] === undefined){
                 gameArray[i] = opponentSymbol;
                 success = true;
             }
-            iteration++;
         }
-        console.log(iteration);
-        printBoard();
+        checkWin();
 
     }
 
@@ -46,20 +48,59 @@ const gameBoard = (() => {
         [].forEach.call(gameTiles, gameTile => {
             if (gameArray[i] !== undefined){
                 gameTile.innerHTML = gameArray[i];
-            }   
+            }   else gameTile.innerHTML = "";
             i++;
-    });
+        });
+    }
+
+    const newPlayer = () => {
+
+    }
 
 
     const checkWin = () => {
-
+       let winningSymbol = "";
+       printBoard();
+       if (gameArray[0] === gameArray[1] && gameArray[1] === gameArray[2]){
+            winningSymbol = gameArray[0];
+        } else if (gameArray[3] === gameArray[4] && gameArray[4] === gameArray[5]){
+            winningSymbol = gameArray[3];
+        } else if (gameArray[6] === gameArray[7] && gameArray[7] === gameArray[8]){
+            winningSymbol = gameArray[6];
+        } else if (gameArray[0] === gameArray[3] && gameArray[3] === gameArray[6]){
+            winningSymbol = gameArray[0];
+        } else if (gameArray[1] === gameArray[4] && gameArray[4] === gameArray[7]){
+            winningSymbol = gameArray[1];
+        } else if (gameArray[2] === gameArray[5] && gameArray[5] === gameArray[8]){
+            winningSymbol = gameArray[2];
+        } else if (gameArray[0] === gameArray[4] && gameArray[4] === gameArray[8]){
+            winningSymbol = gameArray[0];
+        } else if (gameArray[2] === gameArray[4] && gameArray[4] === gameArray[6]){
+            winningSymbol = gameArray[2];
+        }
+        winMsg(winningSymbol);
+        
     }
-}
+
+    const winMsg = (winSym) => {
+        switch (winSym){
+            case "X":
+                gameMessage.innerHTML = "X Wins!";
+                playArea.classList.add("disabled");
+                break;
+            case "O":
+                gameMessage.innerHTML = "O Wins!"
+                playArea.classList.add("disabled");
+                break;
+
+        }
+    }
 
 
 return{
     clearBoard,
-    playerMove
+    playerMove,
+    newPlayer
 
 }
 
@@ -68,3 +109,4 @@ return{
 const player = (name, turnOrder, symbol, color) => {
     return { name, turnOrder, symbol, color};
 };
+
